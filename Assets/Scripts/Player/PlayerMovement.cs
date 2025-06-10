feb5238cf1ff1f12af6869899e11994785f6dfe0
Vitaliy <126505773+Vitaljano-Shev@users.noreply.github.com>
2025-06-10 18:26:53 +0300

    Implemented Touch and Mouse input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float _forwardSpeed;
-    [SerializeField] private float _horizontalSensivity, _horizontalLimit;
+    [SerializeField] private float _horizontalLimit;
+    [SerializeField] private float _horizontalSensivityKeyboard, _horizontalSensivityPoint;
 
     private float _horizontalValue, _valueX;
     private Vector3 _targetPosition;
+    private Camera _camera;
 
     private void Start()
     {
         _targetPosition = transform.position;
+        _camera = Camera.main;
     }
     void Update()
     {
         MoveForward();
-        MoveLeftRight();
+        MoveLeftRightKeyboard();
+        MoveLeftRightPoint();
     }
     
     private void MoveForward()
@@ -24,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.forward * _forwardSpeed * Time.deltaTime);
     }
 
-    private void MoveLeftRight()
+    private void MoveLeftRightKeyboard()
     {
         _horizontalValue = Input.GetAxis("Horizontal");
 
@@ -44,10 +49,43 @@ public class PlayerMovement : MonoBehaviour
                 break;
         }
 
-        _targetPosition += Vector3.right * _valueX * _horizontalSensivity * Time.deltaTime;
-        _targetPosition.x = Mathf.Clamp(_targetPosition.x, -_horizontalLimit, _horizontalLimit);
+        //if (_valueX != 0f )
+        //{
+            _targetPosition += Vector3.right * _valueX * _horizontalSensivityKeyboard * Time.deltaTime;
+            _targetPosition.x = Mathf.Clamp(_targetPosition.x, -_horizontalLimit, _horizontalLimit);
 
-        Vector3 limitedTargetPosition = new Vector3(_targetPosition.x, transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, limitedTargetPosition, 0.2f);
+            Vector3 limitedTargetPosition = new Vector3(_targetPosition.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, limitedTargetPosition, 0.2f); 
+       // }
+    }
+    private void MoveLeftRightPoint()
+    {
+        Vector2 screenPosition = Vector2.zero;
+        bool isActive = false;
+        if(Input.touchCount > 0)
+        {
+            screenPosition = Input.GetTouch(0).position;
+            isActive = true;
+        }
+        else if (Mouse.current.leftButton.isPressed)
+        {
+            screenPosition = Mouse.current.position.ReadValue();
+            isActive = true;
+        }
+
+        if (!isActive) return;
+
+        Ray ray = _camera.ScreenPointToRay(screenPosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        if(plane.Raycast(ray, out float enter))
+        {
+            Vector3 worldTargetPosition = ray.GetPoint(enter);
+            worldTargetPosition.x = Mathf.Clamp(worldTargetPosition.x, -_horizontalLimit, _horizontalLimit);
+
+            Vector3 limitedWorldTargetPosition = new Vector3(worldTargetPosition.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, limitedWorldTargetPosition, _horizontalSensivityPoint);
+            _targetPosition = transform.position;
+        }
     }
 }
